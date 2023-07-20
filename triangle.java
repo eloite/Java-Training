3e824381923560d2983261785f64283d1e1904a7
eloite <104673414+eloite@users.noreply.github.com>
2023-07-20 14:16:58 +0530

    day 1

---
+++ triangle.java
@@ -0,0 +1,12 @@
+public class triangle
+{
+    public static void main(String args[])
+    {
+        int l=8;
+        int b=10;
+        int prod;
+        prod=l*b;
+        System.out.println("the triangle is"+prod);
+    }
+}
+        
